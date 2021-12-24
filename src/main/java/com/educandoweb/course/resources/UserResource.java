52bdf4b1085cc9bfbc3840949bb4ba58a3f58a00
Felipesjc17 <felipe_sjc17@hotmail.com>
2021-12-24 15:41:28 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -35,7 +35,7 @@ public class UserResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = service.finfById(id);
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
